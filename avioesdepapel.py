7aa3542b003316bd6b30e111a92b75350c04b479
Saulo Teixeira <saulotduarte@gmail.com>
2020-11-09 18:20:00 -0300

    corrige a entrada de dados

--- avioesdepapel.py
+++ avioesdepapel.py
@@ -1,12 +1,19 @@
-c=int(input("Número de competidores: "))
-p=int(input("Número de folhas de papel especial: "))
-f=int(input("Número de folhas que cada competidor irá receber: "))
+c, p, f = input().split()
+c = int(c)
+p = int(p)
+f = int(f)
 
-if f*c<=p:
-    print("s")
 
-elif (1<=c<=1000) or (1<=p<=1000) or (1<=f<=1000):
-    print("Há algum valor inválido nas informações que você digiou. Digite valores entre 1 e 1000")
-    
+if (1<=c<=1000) and (1<=p<=1000) and (1<=f<=1000):
+
+    if f*c<=p:
+        print("s")
+
+    else:
+        print("n")
+
 else:
-    print("n")
+    print("Há algum valor inválido nas informações que você digiou. Digite valores entre 1 e 1000")
+
+
+
